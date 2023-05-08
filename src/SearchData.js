d15fb21d678c2dfa8d94eaacbc11a3fe0ba35c3e
Sheharyark <sheharyark62@gmail.com>
2023-05-08 15:03:55 +0530

    first commit

---
+++ src/SearchData.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from 'react'
+import './SearchData.css'
+
+const key = 'fbf712a5a83d7305c3cda4ca8fe7ef29'
+function SearchData() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [location, setLocation] = useState('')
+  const [weatherData, setWeatherData] = useState(null)
+  const [error, setError] = useState(null)
+
+  const handleSubmit = (e) => {
+    setIsLoading(true)
+    e.preventDefault()
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data)
+        console.log(data)
+      })
+      .catch((err) => setError(error.message))
+    setIsLoading(false)
+  }
+
+  return (
+    <div className='searchData'>
+      <form className='searchData__weather' onSubmit={handleSubmit}>
+        <div className='searchData__input'>
+          <label htmlFor='location'>Enter location:</label>
+          <input
+            type='text'
+            id='location'
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+        <button type='submit'>Get Weather</button>
+      </form>
+      {isLoading && <p>Loading...</p>}
+      {weatherData && (
+        <div className='weatherData'>
+          <h2>Weather for {weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp} °C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+          <p>Wind Speed : {weatherData.wind.speed} km/hr</p>
+          <p>Humidity : {weatherData.main.humidity}%</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default SearchData
---
+++ src/api-key.js
@@ -0,0 +1,6 @@
+export const key = '8234a90ac399ce2e2276ba21e384e0e8'
+export const api = `https://api.openweathermap.org/data/2.5/weather?q=London&amp;appid=${key}&amp;units=metric`
+
+//https://api.openweathermap.org/data/2.5/weather?q=anantnag&appid=9a0d316b943de0dd80c5c11228e215ef
+
+//fbf712a5a83d7305c3cda4ca8fe7ef29
